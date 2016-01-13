7f6c7a0ca3c7994eb79284798841e6ce675b0e55
Steven Cassidy <steven.cassidy@gmail.com>
2016-01-12 21:04:05 -0500

    Add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+content = File.read(Rails.root.join('db', 'c_lee.json'))
+hash = JSON.parse(content)
+years = hash["cast"].map {|movie| movie["release_date"][0,4] }.uniq
+months = hash["cast"].map {|movie| movie["release_date"][0,7] }.uniq
+years.each{ |y| Folder.create!(name: y.to_s) }
+months.each do |m|
+  year = m[0,4]
+  parent = Folder.find_by(name: year.to_s)
+  parent.children.create!(name:m)
+end
+hash["cast"].each do |movie|
+  parent = Folder.find_by(name: movie["release_date"][0,7])
+  title = parent.children.create!(name: movie["title"])
+  title.children.create!(name: movie["character"])
+end
